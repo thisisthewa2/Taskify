10492a7df38c7f55686f5ab357e038a810da49bb
thisisthewa2 <thisisthewa2@gmail.com>
2023-12-18 13:42:03 +0900

    refactor: Logo 컴포넌트 코드 개선

--- src/components/logos/Logo.tsx
+++ src/components/logos/Logo.tsx
@@ -1,19 +1,11 @@
 import { IconLargeLogo } from '@/public/svgs';
 import { IconSmallLogo } from '@/public/svgs';
 
-function SmallLogo() {
-  return <IconSmallLogo className='h-27 w-24 tablet:hidden' />;
-}
-
-function LargeLogo() {
-  return <IconLargeLogo className='hidden h-33 w-109 tablet:block pc:block' />;
-}
-
 function Logo() {
   return (
     <div>
-      <SmallLogo />
-      <LargeLogo />
+      <IconLargeLogo className='hidden fill-primary tablet:inline' />
+      <IconSmallLogo className='fill-primary tablet:hidden' />
     </div>
   );
 }
