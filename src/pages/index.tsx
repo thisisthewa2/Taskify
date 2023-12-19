f15caa0e9afe21b7f83817dc4b5540754398f89d
Geon <geonulim@gmail.com>
2023-12-19 14:34:57 +0900

    chore: 테스트 코드 삭제

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,6 @@
-import useRequest from '@/hooks/useRequest';
 import SideMenu from '@/components/SideMenu';
 
 function Home() {
-  const { data, error } = useRequest({
-    options: {
-      url: '/users/me',
-      method: 'get',
-    },
-  });
-
   return (
     <div>
       <SideMenu />
