7db7b0556d9c131d2662747a2c4eb4ec820b638a
티라노 <kde98892@gmail.com>
2023-12-20 12:07:48 +0900

    Refactor: Chip 컴포넌트 적용

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,10 +1,13 @@
 import Image from 'next/image';
 import { CardProps, Mock_1_6_Cards } from '@/pages/api/mock';
 import { IconCalendar } from '@/public/svgs';
+import ColorChip from './chips/ColorChip';
+import TagChip from './chips/TagChip';
 
 //날짜 타입 그냥 내가 맘대로 씀.. 아마 실제로는 함수 써서 바꿔야 될 듯?
 
 function Card({ data }: { data: CardProps }) {
+  if (!data) return;
   return (
     <div className='card flex flex-col gap-6 tablet:flex-row pc:w-314 pc:flex-col pc:gap-10'>
       {data.imageUrl && <CardImage src={data.imageUrl} />}
@@ -41,14 +44,7 @@ function Tags({ tags }: { tags: string[] }) {
     <div className='flex shrink-0 gap-6'>
       {tags.map((tag, key: number) => {
         //태그 컴포넌트 들어가면 됨. 지금은 내가 임의로 만든거..
-        return (
-          <div
-            key={key}
-            className='flex-center rounded-sm bg-[#E7F7DB] px-6 py-3'
-          >
-            <p className='caption-normal text-green'>{tag}</p>
-          </div>
-        );
+        return <TagChip key={key} str={tag} />;
       })}
     </div>
   );
@@ -80,7 +76,6 @@ function CardInfo({ date, assignee }: CardInfoProps) {
           sizes='100%'
           className='rounded-full object-cover'
           alt='작성자 이미지'
-          priority={true}
         />
       </div>
     </div>
