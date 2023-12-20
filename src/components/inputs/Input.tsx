5d88f7d125ba0a4b2bd143f394c6b194cd5b8c10
kanglocal <develocal25@gmail.com>
2023-12-20 10:41:21 +0900

    feat: textarea, date type input 컴포넌트 생성

--- src/components/inputs/Input.tsx
+++ src/components/inputs/Input.tsx
@@ -1,20 +1,78 @@
-import { ReactNode } from 'react';
+import React, { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 function Input({
-  value,
-  type,
+  title,
+  type = 'text',
+  required,
   children,
   ...rest
 }: {
-  value: string;
-  type: string;
-  children: ReactNode;
+  title: string;
+  value: string | number;
+  type: 'text' | 'date' | 'tag';
+  required: boolean;
+  placeholder: string;
+  children?: React.ReactNode;
+  onInput?: React.ChangeEventHandler<HTMLInputElement>;
 }) {
-  return (
-    <input className='' type={type} value={value}>
-      {children}
-    </input>
-  );
+  const [startDate, setStartDate] = useState('');
+  const inputs: {
+    text: React.ReactNode;
+    date: React.ReactNode;
+    tag: React.ReactNode;
+  } = {
+    text: (
+      <div className='flex flex-col gap-[10px]'>
+        <label className='text-[18px] font-[500]'>
+          {title}
+          {required ? <span className='text-[#5534DA]'> *</span> : ''}
+        </label>
+
+        <input className='input' {...rest}>
+          {children}
+        </input>
+      </div>
+    ),
+    date: (
+      <div className='flex flex-col gap-[10px]'>
+        <label className='text-[18px] font-[500]'>
+          {title}
+          {required ? <span className='text-[#5534DA]'> *</span> : ''}
+        </label>
+        <DatePicker
+          dateFormat='yyyy.MM.dd HH:mm'
+          dateFormatCalendar='yyyy년 MM월'
+          showTimeSelect
+          timeFormat='HH:mm'
+          timeIntervals={30}
+          timeCaption='시간'
+          showPopperArrow={false}
+          fixedHeight
+          className='input'
+          placeholderText='날짜를 입력해 주세요'
+          showIcon
+          selected={startDate}
+          onChange={(date: Date) => setStartDate(date)}
+          icon='fa fa-calendar'
+        />
+      </div>
+    ),
+    tag: (
+      <div className='flex flex-col gap-[10px]'>
+        <label className='text-[18px] font-[500]'>
+          {title}
+          {required ? <span className='text-[#5534DA]'> *</span> : ''}
+        </label>
+
+        <input className='input' {...rest}>
+          {children}
+        </input>
+      </div>
+    ),
+  };
+  return inputs[type];
 }
 
 export default Input;
