a044080f8c7ee9ff90a0179eb3042271f458983f
kanglocal <develocal25@gmail.com>
2023-12-21 04:49:30 +0900

    refactor: react-datepicker 스타일 변경

--- src/components/inputs/Input.tsx
+++ src/components/inputs/Input.tsx
@@ -2,7 +2,7 @@ import React, { ReactNode, useState } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import TagChip from '@/components/chips/TagChip';
-import { IconCalendar, IconClose } from '@/public/svgs';
+import { IconCalendar, IconGreater, IconLess } from '@/public/svgs';
 
 type Props = {
   title?: string;
@@ -44,9 +44,71 @@ function DateInput() {
     e.preventDefault();
   };
   const [value, setValue]: [Date | null, Function] = useState(null);
-
+  const YEARS = Array.from(
+    { length: new Date().getFullYear() + 1 - 2000 },
+    (_, i) => new Date().getFullYear() - i,
+  );
+  const MONTHS = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
   return (
     <DatePicker
+      renderCustomHeader={({
+        date,
+        changeYear,
+        decreaseMonth,
+        increaseMonth,
+        prevMonthButtonDisabled,
+        nextMonthButtonDisabled,
+      }) => (
+        <div className='flex justify-between'>
+          <div>
+            <span className='ml-12 font-bold'>{MONTHS[date.getMonth()]}</span>
+            <select
+              value={date.getFullYear()}
+              className='font-bold outline-none'
+              onChange={({ target: { value } }) => changeYear(+value)}
+            >
+              {YEARS.map((option) => (
+                <option key={option} value={option}>
+                  {option}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className='mr-12 flex gap-12'>
+            <button
+              type='button'
+              onClick={decreaseMonth}
+              className=''
+              disabled={prevMonthButtonDisabled}
+            >
+              <IconLess className='h-12 w-12' />
+            </button>
+            <button
+              type='button'
+              onClick={increaseMonth}
+              className=''
+              disabled={nextMonthButtonDisabled}
+            >
+              <IconGreater className='h-12 w-12' />
+            </button>
+          </div>
+        </div>
+      )}
+      showYearDropdown
+      scrollableYearDropdown
       dateFormat='yyyy.MM.dd HH:mm'
       dateFormatCalendar='yyyy년 MM월'
       showTimeSelect
