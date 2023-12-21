a8e46fb9c1b9a6ba5fa10ee0ea99fa1ac4c4e8f3
namminimi <tbvhdaos@naver.com>
2023-12-21 23:25:01 +0900

    fix:ImageDrop 컴포넌트 수정(interface 이름, 함수명)

--- src/components/image-drop/ImageDrop.tsx
+++ src/components/image-drop/ImageDrop.tsx
@@ -6,31 +6,32 @@ interface ImageDropType {
   type: 'modal' | 'profile';
   columnId?: number;
 }
-interface cardLogoImgType {
+
+interface CardLogoImgType {
   [key: string]: 'modal' | 'profile';
 }
 
-function ImageDrop({ type, columnId }: ImageDropType) {
-  const BOX_ATTRIBUTE = {
-    path: {
-      modal: `columns/${columnId}/card-image`,
-      profile: 'users/me/image',
-    },
-    size: {
-      modal: 'h-76 w-76',
-      profile: 'h-100 w-100 tablet:h-190 tablet:w-190',
-    },
-    dataName: {
-      modal: 'imageUrl',
-      profile: 'profileImageUrl',
-    },
-  };
+const DEFINE_IMAGE_PROPERTIES = (columnId: number | undefined = undefined) => ({
+  path: {
+    modal: `columns/${columnId}/card-image`,
+    profile: 'users/me/image',
+  },
+  size: {
+    modal: 'h-76 w-76',
+    profile: 'h-100 w-100 tablet:h-190 tablet:w-190',
+  },
+  dataName: {
+    modal: 'imageUrl',
+    profile: 'profileImageUrl',
+  },
+});
 
+function ImageDrop({ type, columnId }: ImageDropType) {
   const imageFormData = new FormData();
-  const { data: image, fetch } = useRequest<cardLogoImgType>({
+  const { data: image, fetch } = useRequest<CardLogoImgType>({
     skip: true,
     options: {
-      url: BOX_ATTRIBUTE.path[type],
+      url: DEFINE_IMAGE_PROPERTIES(columnId && columnId).path[type],
       method: 'post',
       data: imageFormData,
     },
@@ -44,7 +45,7 @@ function ImageDrop({ type, columnId }: ImageDropType) {
   };
 
   return (
-    <div className={BOX_ATTRIBUTE.size[type]}>
+    <div className={DEFINE_IMAGE_PROPERTIES().size[type]}>
       <label>
         <input
           className='hidden'
@@ -52,13 +53,15 @@ function ImageDrop({ type, columnId }: ImageDropType) {
           type='file'
           onChange={handleChangeImage}
         />
-        <figure className={`group relative ${BOX_ATTRIBUTE.size[type]}`}>
-          {image?.[BOX_ATTRIBUTE.dataName[type]] && (
+        <figure
+          className={`group relative ${DEFINE_IMAGE_PROPERTIES().size[type]}`}
+        >
+          {image?.[DEFINE_IMAGE_PROPERTIES().dataName[type]] && (
             <div className='absolute-center z-base h-full w-full'>
               <Image
                 className='rounded-md group-hover:brightness-75'
                 fill
-                src={image[BOX_ATTRIBUTE.dataName[type]]}
+                src={image[DEFINE_IMAGE_PROPERTIES().dataName[type]]}
                 alt='BannerImage'
               />
               <IconEditLogo className='absolute-center invisible z-nav group-hover:visible' />
