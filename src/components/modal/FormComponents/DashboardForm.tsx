3d94d9cca6ec221cb4046ee39ea59305987636c8
thisisthewa2 <thisisthewa2@gmail.com>
2023-12-26 17:12:06 +0900

    fix: colorchip이 상위컴포넌트로 색상을 전달하도록 변경

--- src/components/modal/FormComponents/DashboardForm.tsx
+++ src/components/modal/FormComponents/DashboardForm.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState } from 'react';
 import { Button } from '@/components/buttons';
 import ColorChip from '@/components/chips/ColorChip';
 import Input from '@/components/inputs/Input';
@@ -8,6 +8,17 @@ interface Props {
 }
 
 function DashboardForm({ onCloseModal }: Props) {
+  const [selectedColor, setSelectedColor] = useState('');
+
+  const handleColorSelection = (color: string) => {
+    setSelectedColor(color); // ColorChip으로부터 선택된 색상을 상태로 설정
+  };
+
+  const handleCreateDashboard = () => {
+    // 선택된 색상 및 다른 정보를 사용하여 작업 수행
+    console.log('Selected color:', selectedColor); 
+    // 여기서 선택된 색상과 input 내용을 이용해 작업을 수행할 수 있음
+  };
   return (
     <>
       <h1 className='heading1-bold'>새로운 대시보드</h1>
@@ -18,7 +29,7 @@ function DashboardForm({ onCloseModal }: Props) {
           placeholder='이름을 입력해 주세요'
         />
         <div className='py-20'>
-          <ColorChip />
+          <ColorChip onSelectColor={handleColorSelection} />
         </div>
       </div>
 
@@ -26,7 +37,7 @@ function DashboardForm({ onCloseModal }: Props) {
         <Button.Secondary size='lg' onClick={onCloseModal}>
           취소
         </Button.Secondary>
-        <Button size='lg' onClick={onCloseModal}>
+        <Button size='lg' onClick={handleCreateDashboard}>
           생성
         </Button>
       </div>
