ce4dc7c3f05249ec9f6a34f61680a0b15c95d405
thisisthewa2 <thisisthewa2@gmail.com>
2023-12-26 10:21:41 +0900

    fix: 대시보드 목록 페이지네이션 수정

--- src/containers/MyDashboard/components/MyDashboardButtons.tsx
+++ src/containers/MyDashboard/components/MyDashboardButtons.tsx
@@ -6,28 +6,37 @@ import ArrowButtonPageChange from './ArrowButtonPageChange';
 import MyDashboardButton from './MyDashboardButton';
 
 interface ButtonProps {
-  data: DashboardProps[] | [];
-  totalCount: number;
+  data: DashboardProps[] | undefined;
+  totalCount: number | undefined;
 }
 
 function MyDashboardButtons({ data, totalCount }: ButtonProps) {
   const [currentPage, setCurrentPage] = useState(1);
+  const totalCountAvailable = totalCount !== undefined && totalCount > 0;
+  const itemsPerPage = 6; // 페이지당 표시할 아이템 개수
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex =
+    currentPage == 1
+      ? startIndex + itemsPerPage - 1
+      : startIndex + itemsPerPage;
 
   return (
     <div className='grid grid-flow-row gap-8 tablet:grid-cols-2 pc:grid-cols-3'>
-      <DashboardButton size='lg'>
-        <p className='pr-12'>새로운 대시보드</p>
-        <AddChip />
-      </DashboardButton>
-      {totalCount > 0 &&
-        data.map((dashBoard, key: number) => {
-          return (
+      {currentPage == 1 && ( //페이지 번호가 1인 경우에만 대시 보드 추가 버튼 보이게
+        <DashboardButton size='lg'>
+          <p className='pr-12'>새로운 대시보드</p>
+          <AddChip />
+        </DashboardButton>
+      )}
+      {totalCountAvailable &&
+        data
+          ?.slice(startIndex, endIndex) // 페이지 범위에 맞게 슬라이싱
+          .map((dashBoard, key: number) => (
             <div key={key}>
               <MyDashboardButton data={dashBoard} />
             </div>
-          );
-        })}
-      {totalCount > 0 && (
+          ))}
+      {totalCountAvailable && (
         <ArrowButtonPageChange
           totalCount={totalCount}
           currentPage={currentPage}
