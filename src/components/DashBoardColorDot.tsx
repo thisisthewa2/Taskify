2085b997dbe696dc8dc4f555c615ecabae8d5238
thisisthewa2 <thisisthewa2@gmail.com>
2023-12-26 10:26:14 +0900

    fix: color 형식 변경

--- src/components/DashBoardColorDot.tsx
+++ src/components/DashBoardColorDot.tsx
@@ -1,17 +1,10 @@
-import { DashboardProps } from '@/pages/api/mock';
-
-function DashBoardColorDot({ color }: DashboardProps) {
-  const dotColor = {
-    green: 'bg-green',
-    primary: 'bg-primary',
-    orange: 'bg-orange',
-    blue: 'bg-blue',
-    pink: 'bg-pink',
-  };
-
+function DashBoardColorDot({ color }: { color: string }) {
   return (
     <div className='flex p-16 tablet:px-12 tablet:py-0 '>
-      <div className={`h-8 w-8 rounded-full ${dotColor[color]}`} />
+      <div
+        className='h-4 w-4 rounded-full'
+        style={{ backgroundColor: color }}
+      ></div>
     </div>
   );
 }
