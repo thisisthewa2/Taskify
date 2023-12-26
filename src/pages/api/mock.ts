b32b8b3dc1a3174259e462598e4734af30df3f5a
thisisthewa2 <thisisthewa2@gmail.com>
2023-12-26 10:22:21 +0900

    fix: 불필요한 mock 데이터 삭제

--- src/pages/api/mock.ts
+++ src/pages/api/mock.ts
@@ -1,83 +1,22 @@
 export interface DashboardsProps {
-  cursorId: number;
-  totalCount: number;
-  dashboards: DashboardProps[];
+  cursorId: number | undefined;
+  totalCount: number | undefined;
+  dashboards: DashboardProps[] | undefined;
 }
 
 export interface DashboardProps {
   id?: number;
   title?: string;
-  color: 'green' | 'orange' | 'blue' | 'primary' | 'pink';
+  color: string;
   createdAt?: string;
   updatedAt?: string;
   createdByMe?: boolean;
   userId?: number;
 }
 
-export const Mock_1_6_dashboards: DashboardsProps = {
-  cursorId: 0,
-  totalCount: 6,
-  dashboards: [
-    {
-      id: 1,
-      title: '핏치핏치',
-      color: 'pink',
-      createdAt: '2023-12-15T03:59:20.817Z',
-      updatedAt: '2023-12-15T03:59:20.817Z',
-      createdByMe: false,
-      userId: 1,
-    },
-    {
-      id: 2,
-      title: '김다은과 아이들',
-      color: 'blue',
-      createdAt: '2023-12-17T03:59:20.817Z',
-      updatedAt: '2023-12-17T03:59:20.817Z',
-      createdByMe: true,
-      userId: 2,
-    },
-    {
-      id: 3,
-      title: '프로덕트 디자인',
-      color: 'green',
-      createdAt: '2023-12-17T03:59:20.817Z',
-      updatedAt: '2023-12-17T03:59:20.817Z',
-      createdByMe: false,
-      userId: 2,
-    },
-    {
-      id: 4,
-      title: '새로운 기획 문서',
-      color: 'primary',
-      createdAt: '2023-12-17T03:59:20.817Z',
-      updatedAt: '2023-12-17T03:59:20.817Z',
-      createdByMe: true,
-      userId: 2,
-    },
-    {
-      id: 5,
-      title: '아함',
-      color: 'pink',
-      createdAt: '2023-12-17T03:59:20.817Z',
-      updatedAt: '2023-12-17T03:59:20.817Z',
-      createdByMe: true,
-      userId: 2,
-    },
-    {
-      id: 6,
-      title: '졸려',
-      color: 'blue',
-      createdAt: '2023-12-17T03:59:20.817Z',
-      updatedAt: '2023-12-17T03:59:20.817Z',
-      createdByMe: false,
-      userId: 2,
-    },
-  ],
-};
-
 export interface InvitationsProps {
-  cursorId: number;
-  invitations: InvitationProps[];
+  cursorId: number | undefined;
+  invitations: InvitationProps[] | undefined;
 }
 
 export interface InvitationProps {
@@ -259,44 +198,6 @@ export interface DashboardsInvitationProps {
   createdAt: string;
   updatedAt: string;
 }
-
-export const Mock_dashboards_dashboardId_invitations = {
-  totalCount: 0,
-  invitations: [
-    {
-      id: 0,
-      inviterUserId: 0,
-      teamId: 'string',
-      dashboard: {
-        title: 'string',
-        id: 0,
-      },
-      invitee: {
-        nickname: '김다은',
-        id: 0,
-      },
-      inviteAccepted: true,
-      createdAt: '2023-12-19T16:17:44.135Z',
-      updatedAt: '2023-12-19T16:17:44.135Z',
-    },
-    {
-      id: 0,
-      inviterUserId: 0,
-      teamId: 'string',
-      dashboard: {
-        title: 'string',
-        id: 0,
-      },
-      invitee: {
-        nickname: '김다은은',
-        id: 0,
-      },
-      inviteAccepted: true,
-      createdAt: '2023-12-19T16:17:44.135Z',
-      updatedAt: '2023-12-19T16:17:44.135Z',
-    },
-  ],
-};
 
 export interface CardsProps {
   cursorId: number;
