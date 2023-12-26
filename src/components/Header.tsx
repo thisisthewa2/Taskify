13ce57aad716496c35774b005125e2e1d1a5cf51
티라노 <kde98892@gmail.com>
2023-12-26 15:20:39 +0900

    fix: Myheader 관리자 권한 유무

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,10 +7,22 @@ import { IconAddBox, IconSettings } from '@/public/svgs';
 import Members from './Members';
 import Logo from './logos/Logo';
 
-function Header() {
+interface Prop {
+  createdByMe?: boolean;
+}
+
+function Header({ createdByMe }: Prop) {
   const loginInfo = useAtomValue(loginAtom);
 
-  return <>{loginInfo.isLoggedIn ? <MyHeader /> : <DefaultHeader />}</>;
+  return (
+    <>
+      {loginInfo.isLoggedIn ? (
+        <MyHeader createdByMe={createdByMe} />
+      ) : (
+        <DefaultHeader />
+      )}
+    </>
+  );
 }
 
 export default Header;
@@ -37,7 +49,7 @@ function TransparentButton({ children }: { children: string }) {
   );
 }
 
-function MyHeader() {
+function MyHeader({ createdByMe }: Prop) {
   const router = useRouter();
   const title = '내 대시보드';
 
@@ -45,7 +57,9 @@ function MyHeader() {
     <div className='flex h-60 items-center justify-between border-b border-solid border-gray-3 bg-white pl-24 pr-12 tablet:h-70 tablet:px-40 pc:pr-80'>
       <div className='heading2-bold pl-4 pt-4'>{title}</div>
       <div className='flex-center body1-normal gap-12 tablet:gap-24'>
-        {router.pathname !== '/mydashboard' && <DashBoardInfo />}
+        {router.pathname !== '/mydashboard' && (
+          <DashBoardInfo createdByMe={createdByMe} />
+        )}
         <ProfileInfo />
       </div>
     </div>
@@ -68,24 +82,26 @@ function ProfileInfo() {
   );
 }
 
-function DashBoardInfo() {
+function DashBoardInfo({ createdByMe }: Prop) {
   return (
     <div className='flex-center h-34 gap-16 border-r border-gray-3 pr-12 text-gray-5 tablet:h-38 tablet:gap-23 tablet:pr-24 pc:gap-40'>
-      <DashboardManageButton />
+      <DashboardManageButton createdByMe={createdByMe} />
       <Members members={Mock_members.members} />
     </div>
   );
 }
 
-function DashboardManageButton() {
+function DashboardManageButton({ createdByMe = false }: Prop) {
   return (
     <div className='flex-center gap-16'>
-      <Button.Outline size='sm'>
-        <div className='hidden pr-8 tablet:block'>
-          <IconSettings />
-        </div>
-        관리
-      </Button.Outline>
+      {createdByMe && (
+        <Button.Outline size='sm'>
+          <div className='hidden pr-8 tablet:block'>
+            <IconSettings />
+          </div>
+          관리
+        </Button.Outline>
+      )}
       <Button.Outline size='sm'>
         <div className='hidden pr-8 tablet:block'>
           <IconAddBox fill='#787486' />
