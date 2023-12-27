c7ce3fbe7f012953134008066ebc4cf4920d8b1b
Geon <geonulim@gmail.com>
2023-12-27 20:42:07 +0900

    feat: 대시보드 이름 변경 박스 구현

--- src/containers/MyDashboard/edit.tsx
+++ src/containers/MyDashboard/edit.tsx
@@ -1,10 +1,46 @@
-import {
-  Mock_1_6_Invitations,
-  Mock_dashboards_dashboardId_invitations,
-  Mock_members,
-} from '@/pages/api/mock';
+import { Mock_1_6_Invitations } from '@/pages/api/mock';
+import { Button } from '@/components/buttons';
+import ColorChip from '@/components/chips/ColorChip';
 import Table from '@/components/tables';
 import { IconArrowBackward } from '@/public/svgs';
+
+const Mock_dashboards_dashboardId_invitations = {
+  totalCount: 0,
+  invitations: [
+    {
+      id: 0,
+      inviterUserId: 0,
+      teamId: 'string',
+      dashboard: {
+        title: 'string',
+        id: 0,
+      },
+      invitee: {
+        nickname: '김다은',
+        id: 0,
+      },
+      inviteAccepted: true,
+      createdAt: '2023-12-19T16:17:44.135Z',
+      updatedAt: '2023-12-19T16:17:44.135Z',
+    },
+    {
+      id: 0,
+      inviterUserId: 0,
+      teamId: 'string',
+      dashboard: {
+        title: 'string',
+        id: 0,
+      },
+      invitee: {
+        nickname: '김다은은',
+        id: 0,
+      },
+      inviteAccepted: true,
+      createdAt: '2023-12-19T16:17:44.135Z',
+      updatedAt: '2023-12-19T16:17:44.135Z',
+    },
+  ],
+};
 
 function DashboardEdit() {
   const { totalCount, invitations: data2 } =
@@ -17,7 +53,7 @@ function DashboardEdit() {
         <IconArrowBackward fill='#333236' />
         돌아가기
       </button>
-      <div className='box'></div>
+      <TitleManageBox />
       <Table type='member' totalCount={2} data={data2} />
       <Table type='dashboard' data={data3} totalCount={6} />
     </div>
@@ -25,3 +61,21 @@ function DashboardEdit() {
 }
 
 export default DashboardEdit;
+
+function TitleManageBox() {
+  return (
+    <div className='flex flex-col gap-30 rounded-lg bg-white px-16 py-32'>
+      <div className='flex items-center justify-between'>
+        <span className='heading1-bold'>비브리지</span>
+        <ColorChip />
+      </div>
+      <label className='subheading-normal'>
+        대시보드 이름
+        <input placeholder='' className='input mt-10' />
+      </label>
+      <div className='flex justify-end'>
+        <Button>변경</Button>
+      </div>
+    </div>
+  );
+}
