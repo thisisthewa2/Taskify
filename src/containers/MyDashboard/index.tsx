20b2ecf54371ee13f8d1c20430ed94cd5a79eea3
thisisthewa2 <thisisthewa2@gmail.com>
2023-12-27 12:03:42 +0900

    fix: 상태관리

--- src/containers/MyDashboard/index.tsx
+++ src/containers/MyDashboard/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import useRequest from '@/hooks/useRequest';
 import { InvitationsProps } from '@/pages/api/mock';
 import { DashboardsProps } from '@/pages/api/mock';
@@ -17,10 +18,20 @@ function MyDashboard({ cursorId, totalCount, dashboards }: DashboardsProps) {
     },
   });
 
+  const [trigger, setTrigger] = useState<number>(0);
+  useEffect(() => {
+    // trigger 상태가 변경될 때마다 실행되는 부분
+    // 추가적인 로직이 있을 경우 여기에 구현
+    console.log('Trigger changed:', trigger);
+  }, [trigger]); // trigger 상태가 변경될 때마다 실행
   const invitations = invitationsData?.invitations;
   return (
     <div className='flex max-h-fit min-h-screen w-full max-w-[64rem] flex-col gap-24 p-24 tablet:gap-44 tablet:p-40'>
-      <MyDashboardButtons data={dashboards} totalCount={totalCount} />
+      <MyDashboardButtons
+        data={dashboards}
+        totalCount={totalCount}
+        setPostTrigger={setTrigger}
+      />
       <Table
         type='dashboard'
         data={invitations}
