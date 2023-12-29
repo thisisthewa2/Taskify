128f9ea103753ee19585d8e181a278de0ebbd46f
thisisthewa2 <thisisthewa2@gmail.com>
2023-12-29 14:05:20 +0900

    fix: landing페이지 파일명 변경

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,7 @@
-import { useAtom } from 'jotai';
-import useRequest from '@/hooks/useRequest';
-import { loginAtom } from '@/store/loginAtom';
-import { setAccessToken } from '@/services/utils/handleToken';
-import SideMenu from '@/components/SideMenu';
+import Landing from '@/containers/Landing';
 
-function Home() {
-  const [loginInfo, setLoginInfo] = useAtom(loginAtom);
-
-  const { data: myInfo } = useRequest({
-    options: {
-      url: 'users/me',
-      method: 'get',
-    },
-  });
-
-  const { data, fetch }: any = useRequest({
-    skip: true,
-    options: {
-      url: 'dashboards',
-      method: 'post',
-      data: {
-        title: '테스트',
-        color: '#7AC555',
-      },
-    },
-  });
-
-  const handleClick = async () => {
-    console.log(myInfo);
-    await fetch();
-    console.log(data);
-  };
-
-  return <button onClick={handleClick}>click</button>;
+function LandingPage() {
+  return <Landing />;
 }
 
-export default Home;
+export default LandingPage;
--- src/pages/landing.tsx
+++
@@ -1,7 +0,0 @@
-import Landing from '@/containers/Landing';
-
-function LandingPage() {
-  return <Landing />;
-}
-
-export default LandingPage;
