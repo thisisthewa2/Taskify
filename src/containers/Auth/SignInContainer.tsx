bcda7a67a6ae5f7111735b8ace465e0ac241aaaa
thisisthewa2 <thisisthewa2@gmail.com>
2023-12-29 18:33:30 +0900

    fix: import 추가

--- src/containers/Auth/SignInContainer.tsx
+++ src/containers/Auth/SignInContainer.tsx
@@ -1,4 +1,4 @@
-import { useSetAtom } from 'jotai';
+import { useAtom, useSetAtom } from 'jotai';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
