71f5fe06b5778d5d6f2b826276485b876e202032
Geon <geonulim@gmail.com>
2023-12-31 04:38:42 +0900

    fix: mypage 스타일 수정

--- src/containers/MyPage/index.tsx
+++ src/containers/MyPage/index.tsx
@@ -4,7 +4,7 @@ import ProfileManageBox from './components/ProfileManageBox';
 
 function MyPage() {
   return (
-    <div className='flex flex-col gap-12 p-12 pt-18'>
+    <div className='flex max-w-[40rem] flex-col gap-12 p-12 pt-18'>
       <button className='flex-center body2-normal mb-8 w-80 gap-6'>
         <IconArrowBackward fill='#333236' />
         돌아가기
