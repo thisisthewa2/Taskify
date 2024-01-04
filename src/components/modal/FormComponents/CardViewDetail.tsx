b50998fc8988981a50535f08387330e88790e235
Geon <geonulim@gmail.com>
2024-01-05 05:50:26 +0900

    fix: import 경로 수정

--- src/components/modal/FormComponents/CardViewDetail.tsx
+++ src/components/modal/FormComponents/CardViewDetail.tsx
@@ -13,7 +13,6 @@ import useRequest from '@/hooks/useRequest';
 import { ColumnsAtom } from '@/store/columnsAtom';
 import { CommentAtom } from '@/store/commentAtom';
 import { closeAllModals, openModal } from '@/store/modalAtom';
-import { CardProps } from '@/pages/api/mock';
 import { changedAtom } from '@/containers/Dashboard/DashboardId';
 import Members from '@/components/Members';
 import { Button } from '@/components/buttons';
