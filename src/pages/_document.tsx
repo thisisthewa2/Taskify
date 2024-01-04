194354c94d15d94be52e9dda9a1ff9518928eefd
thisisthewa2 <thisisthewa2@gmail.com>
2024-01-04 23:03:52 +0900

    fix: favicon 삭제 및 수정

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,25 +4,7 @@ export default function Document() {
   return (
     <Html lang='ko' data-theme='light'>
       <Head>
-        <link id='favicon' rel='icon' href='/svgs/colorcon-1.svg' />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-                var faviconRoute = '/svgs/';
-                var faviconNames = [
-                  'colorcon-1.svg', 'colorcon-2.svg', 'colorcon-3.svg',
-                  'colorcon-4.svg', 'colorcon-5.svg', 'colorcon-6.svg','colorcon-7.svg',
-                ];
-
-                var idx = 0;
-
-                setInterval(function() {
-                  document.querySelector("#favicon").setAttribute('href', faviconRoute + faviconNames[idx++]);
-                  idx %= faviconNames.length;
-                }, 150);
-              `,
-          }}
-        />
+        <link id='favicon' rel='icon' href='/svgs/small-logo.svg' />
       </Head>
       <body>
         <Main />
